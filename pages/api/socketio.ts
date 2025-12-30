2852b3ae929b99b7f487cb93d64535cf93f1d563
faizan1699 <faizanrasheed169@gmail.com>
2025-12-30 15:49:24 +0500

    sdf

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -5,7 +5,6 @@ const SocketHandler = (req: any, res: any) => {
   if (!res.socket.server.io) {
     const io = new Server(res.socket.server)
     res.socket.server.io = io
-
     io.on('connection', (socket) => {
       console.log('New socket', socket.id)
       socket.on('message', (msg) => {
