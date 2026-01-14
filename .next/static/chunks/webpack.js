bef118c8a6942dc1ae4a9966f3e60e216ca91c9c
faizan1699 <faizanrasheed169@gmail.com>
2026-01-14 15:03:05 +0500

    add packages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f5ce489c5bd074b"; }
+/******/ 		__webpack_require__.h = function() { return "8d37d9582d957d6d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
