714518e78cf893b70d743a31759b99be1adb764d
faizan1699 <faizanrasheed169@gmail.com>
2026-01-14 14:55:59 +0500

    add packaged

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd67b98e75afebcd"; }
+/******/ 		__webpack_require__.h = function() { return "0f5ce489c5bd074b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fdde348bd8d7822c"; }
+/******/ 	__webpack_require__.h = function() { return "0f5ce489c5bd074b"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
